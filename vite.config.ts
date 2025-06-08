6f90c67f8ff89d6c4905686f34ce5ac83f1f4a06
Benjamin-dev216 <golden.dev.216@gmail.com>
2025-06-08 06:28:22 -0700

    remove alchemyts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import wasm from "vite-plugin-wasm";
+import rollupNodePolyFill from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [
+    react(),
+    wasm(),
+    {
+      ...rollupNodePolyFill(),
+      enforce: "post",
+    },
+  ],
+  base: "/",
   define: {
     global: "globalThis", // ✅ Needed for some crypto libs
     "process.env": {},
